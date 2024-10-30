752f35726acf157a1836e0d85ebd9e28408e2edf
paaradiso <paaradiso@swag.horse>
2024-10-31 02:05:19 +1030

    increased page width to 64rem

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 	let { children } = $props();
 </script>
 
-<div class="container flex flex-col justify-center items-center my-16 max-w-[48rem]">
+<div class="container flex flex-col justify-center items-center my-16 max-w-[64rem]">
 	<img src={logoUrl} alt="logo" class="h-8" />
 	{@render children()}
 </div>
