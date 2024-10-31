17149ba5c6bed8877fc5a3fc70caeaa8d0a7188c
paaradiso <paaradiso@swag.horse>
2024-10-31 23:40:24 +1030

    add optional google analytics

---
+++ src/lib/components/GoogleAnalytics.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	interface Props {
+		measurementId: string;
+	}
+
+	let { measurementId }: Props = $props();
+
+	const src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+</script>
+
+<svelte:head>
+	<script async {src}>
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			window.dataLayer.push(arguments);
+		}
+		window.gtag = gtag;
+		gtag('js', new Date());
+		gtag('config', measurementId);
+	</script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
 	import '../app.css';
 	import '@fontsource/eb-garamond';
-
+	import GoogleAnalytics from '$lib/components/GoogleAnalytics.svelte';
+	import {
+		PUBLIC_IMAGE_BASE_URL,
+		PUBLIC_LOGO_FILE_NAME,
+		PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID
+	} from '$env/static/public';
 	import urlJoin from 'url-join';
-	import { PUBLIC_IMAGE_BASE_URL, PUBLIC_LOGO_FILE_NAME } from '$env/static/public';
 
 	const logoUrl = urlJoin(PUBLIC_IMAGE_BASE_URL, PUBLIC_LOGO_FILE_NAME);
 
 	let { children } = $props();
 </script>
+
+{#if PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}
+	<GoogleAnalytics measurementId={PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID} />
+{/if}
 
 <div class="container flex flex-col justify-center items-center my-16 max-w-[64rem]">
 	<img src={logoUrl} alt="logo" class="h-8" />
